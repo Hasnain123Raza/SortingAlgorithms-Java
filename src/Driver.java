bcc7f7b4140e65b5d0fd31aa01d6c150ed03348b
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-10-04 12:39:11 -0400

    Added MergeSort

--- src/Driver.java
+++ src/Driver.java
@@ -8,7 +8,8 @@ public class Driver
 	private static SortingAlgorithm sortingAlgorithms[] = {
 		new SelectionSort(),
 		new BubbleSort(),
-		new InsertionSort()
+		new InsertionSort(),
+		new MergeSort()
 	};
 
 	private static Random random = new Random();
